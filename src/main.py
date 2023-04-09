d67777135062eb9387c1da06617c9bce173aabea
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-04-09 17:17:39 -0500

    fix: use os.path instead of local path string

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 import server
 from config import Config
@@ -7,7 +8,8 @@ from generator import Generator
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-    config = Config.load("../config.json")
+    config_path = os.path.join(os.path.dirname(__file__), "../config.json")
+    config = Config.load(config_path)
     generator = Generator(config)
     server = server.Server(config, generator)
     asyncio.get_event_loop().run_until_complete(server.run())
